8f9a2512d7638c114403629d2a275d05d6399e8d
Gurjinder <70706370+GakhalSingh@users.noreply.github.com>
2022-03-23 16:26:17 +0100

    Categories from Database

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,20 +3,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-   /**
-    * @Route("/lucky/number")
-    */
-    public function number() : Response
-    {
-        $pizzas = ['Pizza Salami','Pizza Pepperoni','Pizza Funghi','Pizza Margherita','Pizza Quattro Formaggi','Pizza Tandoori'];
-        return $this->render('sopranos/pizzas.html.twig', [
-            'pizzas' => $pizzas[array_rand($pizzas)],
-        ]);
+    /** * @Route("/home", name="app_show") */
+    public function show(EntityManagerInterface $em) {
+        $repository = $em->getRepository(Category::class);
+        /** @var Category Categories */
+        $Categories = $repository->findAll();
+        return $this->render('sopranos/pizzas.html.twig', [ 'Categories' => $Categories, ]);
     }
 }
